a7c3e69a739edfa1598c139980c54b449c54014c
Saida Davis <saihdavis@gmail.com>
2020-08-14 13:09:26 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,26 +1,23 @@
 class Genre 
   
   attr_accessor :name
-  
   @@all = []
   
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self 
   end
   
-  def songs
+  def songs 
     Song.all.select {|song| song.genre == self}
   end
   
-  
+  def artists
+    songs.collect {|song| song.artist}
+  end 
   
   def self.all
-    @@all 
+    @@all
+  
   end
-  
-  
-  
-  
-  
-end
+end 
